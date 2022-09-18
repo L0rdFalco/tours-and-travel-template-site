1538189ff74c5ecda43e38e4e503f82505a24e87
tel3port <carn3lian@gmail.com>
2022-09-18 19:09:02 +0300

    added skeleton methods on viewsController

---
+++ controllers/viewsController.js
@@ -0,0 +1,108 @@
+const usersModel = require("../models/usersModel.js")
+const reviewsModel = require("../models/reviewsModel.js")
+const bookingsModel = require("../models/bookingsModel.js")
+const toursModel = require("../models/toursModel.js")
+
+exports.getOverviewPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "getOverviewPage fail",
+
+        })
+    }
+}
+
+exports.getSingleTourPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "getSingleTourPage fail",
+
+        })
+    }
+}
+
+exports.getAuthWall = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "getAuthWall fail",
+
+        })
+    }
+}
+
+exports.getDashboardPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "getDashboardPage fail",
+
+        })
+    }
+}
+
+exports.getBookingsPanel = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "getBookingsPanel fail",
+
+        })
+    }
+}
+
+exports.getReviewsPanel = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "getReviewsPanel fail",
+
+        })
+    }
+}
+
+exports.getUsersPanel = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "getUsersPanel fail",
+
+        })
+    }
+}
+
+exports.getSecuritypage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "getSecuritypage fail",
+
+        })
+    }
+}
