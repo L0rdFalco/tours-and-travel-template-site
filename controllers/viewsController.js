8d7992b8c63ecaae010a509902a434fdd175a5fb
tel3port <carn3lian@gmail.com>
2022-09-19 01:18:27 +0300

    filled in pug views with code

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -3,34 +3,6 @@ const reviewsModel = require("../models/reviewsModel.js")
 const bookingsModel = require("../models/bookingsModel.js")
 const toursModel = require("../models/toursModel.js")
 
-exports.getOverviewPage = (request, response, next) => {
-    try {
-
-        response.status(200).render("tour-grid")
-
-
-    } catch (error) {
-
-        response.status(400).json({
-            status: "getOverviewPage fail",
-
-        })
-    }
-}
-
-exports.getSingleTourPage = (request, response, next) => {
-    try {
-
-
-    } catch (error) {
-
-        response.status(400).json({
-            status: "getSingleTourPage fail",
-
-        })
-    }
-}
-
 exports.getAuthWall = (request, response, next) => {
     try {
 
@@ -44,6 +16,102 @@ exports.getAuthWall = (request, response, next) => {
     }
 }
 
+eexports.get404Page = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getAboutUsPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getAddDestinationsPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getAddToursPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getBasicSettingsPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getBookingPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getComingSoonPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getContactPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
 exports.getDashboardPage = (request, response, next) => {
     try {
 
@@ -51,51 +119,431 @@ exports.getDashboardPage = (request, response, next) => {
     } catch (error) {
 
         response.status(400).json({
-            status: "getDashboardPage fail",
+            status: " fail",
 
         })
     }
 }
-
-exports.getBookingsPanel = (request, response, next) => {
+exports.getDestinationDetailPage = (request, response, next) => {
     try {
 
 
     } catch (error) {
 
         response.status(400).json({
-            status: "getBookingsPanel fail",
+            status: " fail",
 
         })
     }
 }
-
-exports.getReviewsPanel = (request, response, next) => {
+exports.getDestinationGridSidebarPage = (request, response, next) => {
     try {
 
 
     } catch (error) {
 
         response.status(400).json({
-            status: "getReviewsPanel fail",
+            status: " fail",
 
         })
     }
 }
-
-exports.getUsersPanel = (request, response, next) => {
+exports.getDestinationGridPage = (request, response, next) => {
     try {
 
 
     } catch (error) {
 
         response.status(400).json({
-            status: "getUsersPanel fail",
+            status: " fail",
 
         })
     }
 }
+exports.getDestinationListSidebarPage = (request, response, next) => {
+    try {
 
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getDestinationListPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getFAQsPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getGuideDetailPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getHireGuidePage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getHotelDetailPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getHotelGridSidebarPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getHotelGridPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getHotelListSidebarPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getHotelListPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getInvoicePage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getLoginPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getManageDestinationsPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getManageHotelsPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getManageRestaurantsPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getManageToursPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getMessagesPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getMyProfilePage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getPricingPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getRestaurantDetailPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getRestaurantGridSidebarPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getRestaurantGridPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getRestaurantListSidebarPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getRestaurantListPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getSignupPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getSiteSettingsPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getSuccessPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getSingleTourDetailPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getAllToursGridSidebarPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getAllToursGridPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getAllToursListSidebarPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
+exports.getAllToursListPage = (request, response, next) => {
+    try {
+
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: " fail",
+
+        })
+    }
+}
 exports.getSecuritypage = (request, response, next) => {
     try {
 
