40b4da243da304a1607c7e306ab3f8a38cd59081
tel3port <carn3lian@gmail.com>
2022-09-18 20:00:50 +0300

    added routes and controllers to bookingsRouter

--- routers/bookingsRouter.js
+++ routers/bookingsRouter.js
@@ -4,6 +4,20 @@ const authController = require("../controllers/authController.js")
 
 const BookingsRouter = express.Router()
 
-BookingsRouter.route("book-now").post(bookingsController.createSingleBooking)
+BookingsRouter.use(authController.protect) //protects all tours
+
+BookingsRouter.route("/").get(authController.restrictTo("admin"), bookingsController.getAllBookings)
+BookingsRouter.route("/:userid")
+    .get(authController.restrictTo("admin", "user"),
+        bookingsController.getAllBookings)
+
+BookingsRouter.route("/b/:bookingid")
+    .get(authController.restrictTo("admin", "user"), bookingsController.getSingleBooking)
+    .delete(authController.restrictTo("admin"), bookingsController.deleteSingleBooking)
+
+BookingsRouter.route("book-now").post(
+    authController.restrictTo("admin", "user"),
+    bookingsController.createSingleBooking)
+
 
 module.exports = BookingsRouter
