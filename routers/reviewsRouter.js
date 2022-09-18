eb2cc190e83423074011e804e5427a154f34fd23
tel3port <carn3lian@gmail.com>
2022-09-18 20:11:34 +0300

    added routes and controllers to reviewsRouter

--- routers/reviewsRouter.js
+++ routers/reviewsRouter.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const reviewsController = require("../controllers/reviewsController.js")
+const authController = require("../controllers/authController.js")
 const ReviewsRouter = express.Router()
 
+ReviewsRouter.route("/")
+    .get(reviewsController.getAllReviews)
+    .post(authController.protect, authController.restrictTo("admin", "user"), reviewsController.createSingleReview)
+
+ReviewsRouter.route("/:review-id")
+    .get(reviewsController.getSingleReview)
+    .patch(authController.protect, authController.restrictTo("admin", "user"), reviewsController.updateSingleReview)
+    .delete(authController.protect, authController.restrictTo("admin", "user"), reviewsController.deleteSingleReview)
 module.exports = ReviewsRouter
