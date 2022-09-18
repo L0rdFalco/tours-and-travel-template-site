c35090796be69d19e029834e203f04074c19aa81
tel3port <carn3lian@gmail.com>
2022-09-18 19:02:28 +0300

    added skeleton methods on authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,149 @@
 exports.signup = (request, response, next) => {
-
     try {
 
-    } catch (error) {
-        console.log(error);
-        response.status(400).json({
-            status: "fail",
-            message: "singnup fail"
+        response.status(200).json({
+            status: "signup  success",
+
         })
 
-    }
+    } catch (error) {
 
+        response.status(400).json({
+            status: "signup fail",
+
+        })
+    }
+}
+
+exports.login = (request, response, next) => {
+    try {
+
+        response.status(200).json({
+            status: "login  success",
+
+        })
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "login fail",
+
+        })
+    }
+}
+
+exports.logout = (request, response, next) => {
+    try {
+
+        response.status(200).json({
+            status: "logout  success",
+
+        })
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "logout fail",
+
+        })
+    }
+}
+
+exports.forgotpassword = (request, response, next) => {
+    try {
+
+        response.status(200).json({
+            status: "forgotpassword  success",
+
+        })
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "forgotpassword fail",
+
+        })
+    }
+}
+
+exports.resetpassword = (request, response, next) => {
+    try {
+
+        response.status(200).json({
+            status: "reset  success",
+
+        })
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "reset fail",
+
+        })
+    }
+}
+
+exports.updatepassword = (request, response, next) => {
+    try {
+
+        response.status(200).json({
+            status: "updatepassword  success",
+
+        })
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "updatepassword fail",
+
+        })
+    }
+}
+
+exports.isLoggedIn = (request, response, next) => {
+    try {
+
+        response.status(200).json({
+            status: "isLoggedIn  success",
+
+        })
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "isLoggedIn fail",
+
+        })
+    }
+}
+
+exports.protect = (request, response, next) => {
+    try {
+
+        response.status(200).json({
+            status: "protect  success",
+
+        })
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "protect fail",
+
+        })
+    }
+}
+
+exports.restrictTo = (...roles) => {
+    return (request, response, next) => {
+        try {
+
+            if (!roles.includes(request.user)) return response.status(400).json({ message: "inadequate permissions" })
+
+        } catch (error) {
+
+            console.log("restrictTo failed");
+
+        }
+    }
 }
