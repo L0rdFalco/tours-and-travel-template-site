a285d89abab39a24d293ea844531d25ee74a462f
tel3port <carn3lian@gmail.com>
2022-09-23 08:38:27 +0300

    added code in usersController functions

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,96 +1,140 @@
-const usersModel = require("../models/usersModel.js")
+const UsersModel = require("../models/usersModel.js")
 
-exports.getAllUsers = (request, response, next) => {
+exports.getAllUsers = async (request, response, next) => {
     try {
 
+        const userDocs = await UsersModel.find().select("-__v")
+
         response.status(200).json({
-            status: "get all review s success",
+            status: "get all users success",
+            payload: {
+                data: userDocs
+            }
 
         })
 
     } catch (error) {
 
         response.status(400).json({
-            status: "get all review s fail",
+            status: "get all users fail",
 
         })
     }
 }
 
 
-exports.getSingleUser = (request, response, next) => {
+exports.getSingleUser = async (request, response, next) => {
     try {
 
+        const userDoc = await UsersModel.findById(request.user._id).select("__v")
+
         response.status(200).json({
-            status: "get single review  success",
+            status: "get single user  success",
+
+            payload: {
+                data: userDoc
+            }
 
         })
 
     } catch (error) {
 
         response.status(400).json({
-            status: "get single review  fail",
+            status: "get single user  fail",
 
         })
     }
 }
 
-exports.createSingleUser = (request, response, next) => {
+exports.createSingleUser = async (request, response, next) => {
     try {
 
+        const userData = {
+            name: request.body.name,
+            email: request.body.email,
+            password: request.body.password,
+            passwordConfirm: request.body.passwordConfirm,
+            role: request.body.role
+        }
+
+        const newUser = await UsersModel.create(userData)
+
+        //so that they dont show up in the json response object
+        newUser.password = undefined
+        newUser.isActive = undefined
+        newUser.role = undefined
+        newUser.__v = undefined
         response.status(200).json({
-            status: "create single review  success",
+            status: "create single User success",
+            data: {
+                user: newUser
+            }
+
+        })
+
+    } catch (error) {
+        console.log(error);
+
+        response.status(400).json({
+            status: "create single User fail",
+
+        })
+    }
+
+}
+
+exports.updateSingleUser = async (request, response, next) => {
+    try {
+        const userData = {
+            name: request.body.name,
+            email: request.body.email
+        }
+        const updatedUser = await UsersModel.findByIdAndUpdate(request.user.id, userData, { runValidators: true, new: true, select: "name email photo id" })
+
+        response.status(200).json({
+            status: "update single User success",
+            data: {
+                user: updatedUser
+            }
 
         })
 
     } catch (error) {
 
         response.status(400).json({
-            status: "create single review  fail",
+            status: "update single User fail",
 
         })
     }
 }
 
-exports.updateSingleUser = (request, response, next) => {
+exports.deleteSingleUser = async (request, response, next) => {
     try {
-
+        const deletedUser = await UsersModel.findByIdAndDelete(request.user.id)
         response.status(200).json({
-            status: "update single review  success",
+            status: "delete single User success",
 
         })
 
     } catch (error) {
 
         response.status(400).json({
-            status: "update single review  fail",
+            status: "delete single User fail",
 
         })
     }
+
 }
 
-exports.deleteSingleUser = (request, response, next) => {
+exports.getMyData = async (request, response, next) => {
     try {
+        const singleUser = await UsersModel.findById(request.params.id)
 
         response.status(200).json({
-            status: "delete single review  success",
-
-        })
-
-    } catch (error) {
-
-        response.status(400).json({
-            status: "delete single review fail",
-
-        })
-    }
-}
-
-exports.getMyData = (request, response, next) => {
-    try {
-
-        response.status(200).json({
-            status: "getMyData  success",
+            status: "getMyData success",
+            data: {
+                user: singleUser
+            }
 
         })
 
@@ -103,11 +147,21 @@ exports.getMyData = (request, response, next) => {
     }
 }
 
-exports.updateMyData = (request, response, next) => {
+exports.updateMyData = async (request, response, next) => {
     try {
+        /**
+         * email, profilepic, name, etc
+         */
+
+        const userData = {
+            name: request.body.name,
+            email: request.body.email
+        }
+        const updatedUser = await UsersModel.findByIdAndUpdate(request.user.id, userData, { runValidators: true, new: true })
 
         response.status(200).json({
-            status: "updateMyData  success",
+            status: "updateMyData success",
+            data: updatedUser
 
         })
 
@@ -120,18 +174,19 @@ exports.updateMyData = (request, response, next) => {
     }
 }
 
-exports.deactivateAccount = (request, response, next) => {
+exports.deactivateAccount = async (request, response, next) => {
     try {
+        await UsersModel.findByIdAndUpdate(request.user.id, { isActive: false }, { runValidators: true, new: true })
 
         response.status(200).json({
-            status: "deactivateAccount success",
+            status: "deactivate Account successfully",
 
         })
 
     } catch (error) {
 
         response.status(400).json({
-            status: "deactivateAccount fail",
+            status: "deactivateMyAccount fail",
 
         })
     }
