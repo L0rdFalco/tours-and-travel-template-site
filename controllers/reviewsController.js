8266984c94b9bccde0ab7c38a25337e3dd116f79
tel3port <carn3lian@gmail.com>
2022-09-23 10:16:30 +0300

    added code for reviewsController

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -1,86 +1,121 @@
-const reviewsModel = require("../models/reviewsModel.js")
-exports.getAllReviews = (request, response, next) => {
+const ReviewsModel = require("../models/reviewsModel.js")
+exports.getAllReviews = async (request, response, next) => {
     try {
+        const reviews = await ReviewsModel.find().select("-__v")
 
         response.status(200).json({
-            status: "get all reviews success",
+            status: "get all Reviews success",
+            number: reviews.length,
+            data: {
+                revs: reviews
+            }
 
         })
 
     } catch (error) {
 
         response.status(400).json({
-            status: "get all reviews fail",
+            status: "get all Reviews fail",
+
+        })
+    }
+
+}
+
+
+exports.getSingleReview = async (request, response, next) => {
+    try {
+        const singleReview = await ReviewsModel.findById(request.params.id).select("-__v")
+
+        response.status(200).json({
+            status: "get single Reviews success",
+            data: {
+                rev: singleReview
+            }
+
+        })
+
+    } catch (error) {
+
+        response.status(400).json({
+            status: "get single Reviews fail",
 
         })
     }
 }
 
-
-exports.getSingleReview = (request, response, next) => {
+exports.createSingleReview = async (request, response, next) => {
     try {
 
+        const reviewData = {
+            review: request.body.review,
+            rating: request.body.rating,
+            user: request.body.user,
+            tour: request.body.tour
+
+        }
+
+        const newReview = await ReviewsModel.create(reviewData)
+
         response.status(200).json({
-            status: "get single review success",
+            status: "create single Review success",
+            data: {
+                review: newReview
+            }
 
         })
 
     } catch (error) {
 
         response.status(400).json({
-            status: "get single review fail",
+            status: "create single Review fail",
+
+        })
+    }
+
+}
+
+exports.updateSingleReview = async (request, response, next) => {
+    try {
+        const reviewData = {
+            review: request.body.review,
+            rating: request.body.rating
+        }
+        const updatedReview = await ReviewsModel.findByIdAndUpdate(request.params.id, reviewData, { runValidators: true, new: true })
+
+        response.status(200).json({
+            status: "update single Review success",
+            data: {
+                rev: updatedReview
+            }
+
+        })
+
+    } catch (error) {
+        console.log(error);
+
+        response.status(400).json({
+            status: "update single Review fail",
 
         })
     }
 }
 
-exports.createSingleReview = (request, response, next) => {
+exports.deleteSingleReview = async (request, response, next) => {
     try {
+        const deletedReview = await ReviewsModel.findByIdAndDelete(request.params.id)
 
         response.status(200).json({
-            status: "create single review success",
+            status: "delete single Review success",
 
         })
 
     } catch (error) {
 
         response.status(400).json({
-            status: "create single review fail",
+            status: "delete single Review fail",
 
         })
     }
-}
 
-exports.updateSingleReview = (request, response, next) => {
-    try {
-
-        response.status(200).json({
-            status: "update single review success",
-
-        })
-
-    } catch (error) {
-
-        response.status(400).json({
-            status: "update single review fail",
-
-        })
-    }
-}
-
-exports.deleteSingleReview = (request, response, next) => {
-    try {
-
-        response.status(200).json({
-            status: "delete single review success",
-
-        })
-
-    } catch (error) {
-
-        response.status(400).json({
-            status: "delete single review fail",
-
-        })
-    }
 }
