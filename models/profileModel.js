6e69a803781a6ee823e94d22df26658cbde37736
tel3port <carn3lian@gmail.com>
2022-11-21 15:26:53 +0300

    added profile schema type options

--- models/profileModel.js
+++ models/profileModel.js
@@ -1,7 +1,72 @@
 const mongoose = require("mongoose");
 const profileSchema = mongoose.Schema(
     {
+        firstname: {
+            type: String,
+            required: [true, "profile must have a first name"]
+        },
+        lastname: {
+            type: String,
+            required: [true, "profile must have a last name"]
+        },
+        email: {
+            type: String,
+            required: [true, "profile must have an email"]
+        },
+        aboutme: {
+            type: String,
+            default: "this is about me section"
+        },
+        livein: {
+            type: String,
+            required: [true, "profile must have live in info"]
+        },
+        ispeak: {
+            type: String,
+            required: [true, "profile must provide spoken language info"]
+        },
+        phone: {
+            type: Number,
+            default: 2568168964168
+        },
+        city: {
+            type: String,
+            default: "Monte Carlo"
+        },
+        profilepic: {
+            type: String,
+            default: "user-1.jpg"
+        },
+        facebookurl: {
+            type: String,
 
+        },
+        twitterurl: {
+            type: String,
+
+        },
+        linkedinurl: {
+            type: String
+        },
+        pinteresturl: {
+            type: String
+
+        },
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+
+        createdAt: {
+            type: String,
+            default: Date.now,
+            select: false
+
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User"
+        }
     },
     {
         //allows virtual fields to show up in responses
--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,5 +1,23 @@
 const profileCreateBtn = document.getElementById("profile_create")
 const profileDeleteBtn = document.getElementById("profile_delete")
+const imageUPloadBtn = document.getElementById("aaiu-uploader")
+
+const firstnameEl = document.getElementById("firstname")
+const lastnameEl = document.getElementById("lastname")
+const emailEl = document.getElementById("email")
+const about_meEl = document.getElementById("about_me")
+const live_inEl = document.getElementById("live_in")
+const i_speakEl = document.getElementById("i_speak")
+const phoneEl = document.getElementById("phone")
+const cityEl = document.getElementById("city")
+const usermobileEl = document.getElementById("usermobile")
+const userfacebookEl = document.getElementById("userfacebook")
+const usertwitterEl = document.getElementById("usertwitter")
+const userlinkedinEl = document.getElementById("userlinkedin")
+const userpinterestEl = document.getElementById("userpinterest")
+
+
+
 
 if (profileCreateBtn) profileCreateBtn.addEventListener("click", function (e) {
     e.preventDefault()
