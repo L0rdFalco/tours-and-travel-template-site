4136d9add0eceebe8bb3e860a130b2f13cd60479
tel3port <carn3lian@gmail.com>
2025-05-30 19:15:20 -0300

    getting tours and travel site to work

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-dotenv.config({ path: `./config.env` })
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
-const app = require("./app.js")
+const app = require("./app.js");
 
-mongoose.connect(process.env.DATABASE).then(() => {
-    console.log("db connected!");
-})
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 5000
-app.listen(port, () => {
-    console.log(`natoursD listening on port ${port}`);
-})
+async function nodeServerInit() {
+  while (true) {
+    try {
+      await mongoose.connect(process.env.MONGO_CONN_STR);
+
+      app.listen(port, () => {
+        console.log(`db listening on port ${port}`);
+      });
+      break;
+    } catch (error) {
+      console.log("db connection error");
+    }
+  }
+
+}
+
+nodeServerInit();
--- utils/passport-setup.js
+++ utils/passport-setup.js
@@ -1,14 +1,25 @@
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
+const passport = require("passport")
+const GoogleStrategy = require("passport-google-oauth20")
 const authController = require("../controllers/authController.js")
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://toursandtravel.onrender.com/api/v1/users/gcloud-webhook"
-
-}, authController.passportCallback))
-
+ if (process.env.NODE_ENV === "development") {
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://127.0.0.1:3000/users/googlecloud-webhook",
+    
+    }, authController.passportCallback))
+    
+ }
+else if (process.env.NODE_ENV === "production") {
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "https://toursandtravel.onrender.com/api/v1/users/gcloud-webhook"
+    
+    }, authController.passportCallback))
+    
+}
 
 passport.serializeUser(authController.passportSerialiseUserCB)
 
